b3e0b30e3fb81718afdfad6b5ea8c71255d15c7a
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-26 14:08:16 +0530

    Flip Coin and print percentage of Heads and Tails

---
+++ Basic_Core_Programs/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Basic_Core_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for Flip coin problem");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number of time flips");
+                    int noOfTimes = Convert.ToInt32(Console.ReadLine());
+                    Q1_Flip_Coin flip_Coin = new Q1_Flip_Coin();
+                    flip_Coin.Flip(noOfTimes);
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ Basic_Core_Programs/Q1_Flip_Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    public class Q1_Flip_Coin
+    {
+        public void Flip(int no_Of_Time)
+        { 
+            Random random = new Random();
+            int heads = 0, tails = 0;
+            for (int i = 0; i < no_Of_Time; i++)
+            {
+                double num = random.NextDouble();
+                if (num < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
+            }
+            
+            Console.WriteLine("Percentage of Head = " + ((heads / (float) no_Of_Time))*100);
+            Console.WriteLine("Percentage of Tail = " + ((tails / (float)  no_Of_Time))*100);
+        }
+    }
+}
